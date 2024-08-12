273b721e59e94750e98a9fa08520379af85d4b3e
Puneet Kumar <developerpuneet2010@gmail.com>
2024-08-12 18:55:46 +0530

    Update Load.js

--- public/js/Load.js
+++ public/js/Load.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const video = document.getElementById('bg-video');
     video.playbackRate = 0.5;
 });
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://lemonteams.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/lemonteams.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
