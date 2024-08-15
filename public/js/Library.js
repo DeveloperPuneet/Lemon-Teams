bc31b40e78d341e57c01adfb4e474fced18e655a
Puneet-Kumar2010 <pk.mishra10102010@gmail.com>
2024-08-15 21:39:02 +0530

    update: beta of Library

---
+++ public/js/Library.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search');
+    const libraryCards = document.querySelectorAll('.lib-card');
+
+    searchInput.addEventListener('input', function() {
+        const searchTerm = searchInput.value.toLowerCase();
+        libraryCards.forEach(card => {
+            const textContent = card.querySelector('.hide').textContent.toLowerCase();
+            if (textContent.includes(searchTerm)) {
+                card.parentElement.style.display = ''; 
+            } else {
+                card.parentElement.style.display = 'none';
+            }
+        });
+    });
+});
