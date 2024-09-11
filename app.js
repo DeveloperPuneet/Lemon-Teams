cc72cc6746c6f7238a37816c8f9925ba4ed2f182
Puneet Kumar <developerpuneet2010@gmail.com>
2024-09-11 16:39:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ cron.schedule('*/15 * * * *', () => {
 });
 
 const url = "https://lemonteams.onrender.com";
-const interval = 80000;
+const interval = 150000;
 function reloadWebsite() {
     axios.get(url)
         .then(response => {
