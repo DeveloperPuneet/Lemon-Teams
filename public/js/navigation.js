21df8b684cb84232177eb9224d3160ad5eeeeee5
Puneet Kumar <developerpuneet2010@gmail.com>
2024-10-20 14:54:42 +0000

    error

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -23,29 +23,27 @@ notifyIcon.addEventListener("click", () => {
 
 // JavaScript function to calculate "time ago"
 function timeAgo(date) {
-    setInterval(()=>{
-        let now = new Date();
-        const time = new Date(now);
-        const timestamp = time.getTime();
-        const seconds = Math.floor((timestamp - new Date(Number(date))) / 1000); 
-    
-        let interval = Math.floor(seconds / 31536000);
-        if (interval >= 1) return `${interval} year${interval > 1 ? 's' : ''} ago`;
-    
-        interval = Math.floor(seconds / 2592000);
-        if (interval >= 1) return `${interval} month${interval > 1 ? 's' : ''} ago`;
-    
-        interval = Math.floor(seconds / 86400);
-        if (interval >= 1) return `${interval} day${interval > 1 ? 's' : ''} ago`;
-    
-        interval = Math.floor(seconds / 3600);
-        if (interval >= 1) return `${interval} hour${interval > 1 ? 's' : ''} ago`;
-    
-        interval = Math.floor(seconds / 60);
-        if (interval >= 1) return `${interval} minute${interval > 1 ? 's' : ''} ago`;
-    
-        return `just now`;
-    }, 60);
+    let now = new Date();
+    const time = new Date(now);
+    const timestamp = time.getTime();
+    const seconds = Math.floor((timestamp - new Date(Number(date))) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) return `${interval} year${interval > 1 ? 's' : ''} ago`;
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) return `${interval} month${interval > 1 ? 's' : ''} ago`;
+
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) return `${interval} day${interval > 1 ? 's' : ''} ago`;
+
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) return `${interval} hour${interval > 1 ? 's' : ''} ago`;
+
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) return `${interval} minute${interval > 1 ? 's' : ''} ago`;
+
+    return `just now`;
     // Ensure the date is treated as a valid timestamp
 }
 
