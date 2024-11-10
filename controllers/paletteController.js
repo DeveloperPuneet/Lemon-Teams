a4d003de6af7aa2ee1af7f826db881c0c3833b14
Puneet Kumar <developerpuneet2010@gmail.com>
2024-11-10 12:31:01 +0530

    Update paletteController.js

--- controllers/paletteController.js
+++ controllers/paletteController.js
@@ -471,7 +471,7 @@ setInterval(async () => {
     );
 }, 600000);
 
-cron.schedule('5 0 * * 0', async () => {
+cron.schedule('40 12 * * 0', async () => {
     try {
         await sendTopPalettesEmail();
     } catch (error) {
